5a1e2fa5b96da191978a81942ee69e6861ec722a
etxahun <etxahun.sanchez@gmail.com>
2014-03-24 01:53:48 +0100

    Nada destacable.
    Nada destacable.

--- showpendientes.php
+++ showpendientes.php
@@ -13,7 +13,7 @@
 	}
 	
 	/* Open MySQL connection */
-	$mysqli = new mysqli("localhost", "root", "xxxxxx", "maletaboda" );
+	$mysqli = new mysqli("localhost", "root", "xxxx", "maletaboda" );
 	
 	/* If we are asked to delete a row */
 	if( isset($type) && ($type=="del") && isset($val) ) {
